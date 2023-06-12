e2f0b6a0eea3adcf300fd259c90373730a5e0f5d
jihao <809146012@qq.com>
2023-06-12 19:22:24 +0800

    模板

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 """
 视图
     其实就是python函数
+    
 视图函数有两个要求：
     1. 视图函数的第一个参数就是接收请求。这个请求其实就是 HttpRequest的类对象
     2. 必须返回一个响应
@@ -15,4 +16,16 @@ from django.http import HttpResponse
 
 # 我们希望用户输入 http://127.0.0.1:8000/index/ 来访问视图函数
 def index(request):
-    return HttpResponse('ok')
+    # return HttpResponse('ok')
+
+    # render  渲染模板
+    # request, template_name, context=None,
+    # request , 请求
+    # template_name 模板名字
+    # context=None
+
+    # 模拟数据查询
+    context = {
+        'name': '马上双十一,点击有惊喜'
+    }
+    return render(request, 'book/index.html', context=context)
