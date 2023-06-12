d28f8ffa709a02432035b9d430b2fcb8d25679f4
jihao <809146012@qq.com>
2023-06-12 21:10:58 +0800

    连接mysql数据库

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'book'
+    'book',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'bookmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 123456,
+        'NAME': 'book',
     }
 }
 
